212ee0c349a57de5a913abc3fba181087fc03af7
Mazdak Salavati <MazdaX@users.noreply.github.com>
2018-10-01 17:32:39 +0100

    Update R_must_collection.R
    reusables

--- R_must_collection.R
+++ R_must_collection.R
@@ -4,6 +4,34 @@ set.seed(1234)
 #For reproducable environments
 save.image("ETL.RData")
 
+#Always
+require() #instead of library()
+
+#Mass installations (reusable)
+apps <-
+  c("tidyverse",
+    "FactoMineR",
+    "factoextra",
+    "devtools",
+    "caret",
+    "survminer",
+    "qqman",
+    "ggrepel",
+    "getopt", 
+    "binom", 
+    "VGAM")
+
+for (a in apps) {
+  if ((a %in% rownames(installed.packages())) == FALSE) {
+    install.packages(a, dependencies = TRUE,
+                     lib = "/home/$USER/Rlibrary", #reusable after R engine upgrade by simply pointing to this location
+                     repos = 'http://cran.ma.imperial.ac.uk/', # ONLY if in  UK
+                     destdir = "/home/$USER/RlibraryDownloads", #Not really necessary but sometimes useful to keep the install copy
+                     quiet = TRUE ) 
+  }
+}
+
+
 #Issues with decimal limits (digits could be 1..22 the default is 7 for printing and return)
 options(verbose = TRUE, digits=22)
 
@@ -19,4 +47,8 @@ function(p, eps = 1e-16){
   return(z)
 }
 
-#
+#Converter of choice for many ocasions that numbers are parsed as factor by mistake.
+factor2numeric<- function (f) {
+  as.numeric(as.character(f))
+}
+
