610b200236dd130c707da5a3d0221ee7efd5f6e6
Mazdak Salavati <MazdaX@users.noreply.github.com>
2018-10-01 17:22:51 +0100

    Update R_must_collection.R
    the qnorm mayham

--- R_must_collection.R
+++ R_must_collection.R
@@ -10,4 +10,13 @@ options(verbose = TRUE, digits=22)
 #Installing packages with multiple cores (R v > 3.5) ==> making compilation of gcc compiler much faster e.g. iCore7 8nct
 install.packages("MASS",Ncpus = 8)
 
+#qnorm for polishing the p value distribution before coverting to Z distribution.
+function(p, eps = 1e-16){
+  if(p[which(p > (1-eps))]){
+        z=qnorm(eps,lower.tail=FALSE) #correct approach not 1-eps
+        } else if (p[which(p < eps)]){
+                z=qnorm(eps)}
+  return(z)
+}
+
 #
